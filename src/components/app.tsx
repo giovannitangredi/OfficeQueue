d7c9e0f2936ba0fcd266a120c6df5b68a9ae4ef0
Enrico Carraro <enricocarraro@google.com>
2020-10-16 18:02:43 +0200

    Project Skeleton.

---
+++ src/components/app.tsx
@@ -0,0 +1,18 @@
+// Import deps
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+// Import styles
+import './../styles/styles.css'
+
+// Create App component
+export const App = () => {
+ 
+
+  return (
+    <div className="app">
+      Content
+    </div>
+  )
+}
