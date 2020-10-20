85fa2d3ea492947b7da8b5aadb4d99cf2f4453f3
Federico Quarta <s280113@studenti.polito.it>
2020-10-20 17:44:11 +0200

    fix: adjust the layout for large screen

--- src/components/ServiceSelectionView.js
+++ src/components/ServiceSelectionView.js
@@ -1,29 +1,75 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { ListGroup, Container, Row, Col } from 'react-bootstrap';
-import Api from '../api/Api';
-import moment from 'moment';
 
 function ServiceSelectionView() {
     const [services, setServices] = useState([]);
     const [currentService, setCurrentService] = useState('');
     const [queueLength, setQueueLength] = useState(0);
-    const [waitingTime, setWaitingTime] = useState(0);
 
     useEffect(()=>{
-        Api.getServices()
-            .then((availableServices) => {
-                setServices(availableServices);
-            });
+        const fakeServices = [
+                    {
+                        id: 'S001',
+                        type: 'pay bills'
+                    }, 
+                    {
+                        id: 'S002',
+                        type: 'collect a parcel'
+                    }, 
+                    {
+                        id: 'S003',
+                        type: 'send a parcel'
+                    }
+                ];
+                setServices(fakeServices);
     }, []);
 
     const handleServiceSelection = (serviceID) => {
         setCurrentService(serviceID);
-        Api.getServiceInfo(serviceID)
-            .then((service)=>{
-                setQueueLength(service.queueLength);
-                setWaitingTime(service.waitingTime);
-            });
+        const fakeInfo = [
+            {
+                id: 'S001',
+                type: 'pay bills',
+                queueLength: 10,
+                waitingTime: 72
+            }, 
+            {
+                id: 'S002',
+                type: 'collect a parcel',
+                queueLength: 20,
+                waitingTime: 60
+            }, 
+            {
+                id: 'S003',
+                type: 'send a parcel',
+                queueLength: 5,
+                waitingTime: 25
+            }
+        ];
+    
+        let info = {
+            id: '',
+            type: '',
+            queueLength: -1,
+            waitingTime: -1
+        };
+    
+        switch(serviceID) {
+            case 'S001':
+                info = fakeInfo[0];
+                break;
+            case 'S002':
+                info = fakeInfo[1];
+                break;
+            case 'S003':
+                info = fakeInfo[2];
+                break;
+            default:
+                break;
+    
+        }
+        setQueueLength(info.queueLength);
     }
 
     return (
@@ -32,12 +78,12 @@ function ServiceSelectionView() {
                 <h4 className='mx-auto mt-3 text-center p-2'>Available services</h4>
             </Row>
             <Row >
-                <Col sm={12} lg={6} style={{ maxHeight: '60vh' }} className='overflow-auto p-3'>
+                <Col sm={12} style={{ maxHeight: '60vh' }} className='mx-auto overflow-auto p-3'>
                     
                     <ListGroup variant="flush">
                         {
                             services.map(s =>
-                                <ListGroup.Item active={currentService === s.id ? true : false} onClick={()=>handleServiceSelection(s.id)} action className='rounded-pill mx-auto mt-3 shadow text-center w-75 p-4'> 
+                                <ListGroup.Item key={s.id} active={currentService === s.id ? true : false} onClick={()=>handleServiceSelection(s.id)} action className='rounded-pill mx-auto mt-3 shadow text-center w-75 p-4'> 
                                     {s.type} 
                                 </ListGroup.Item>
                             )
@@ -45,20 +91,23 @@ function ServiceSelectionView() {
                     </ListGroup>
                     
                 </Col>
-                <Col sm={12} lg={6} className="d-flex flex-column justify-content-center">
+            </Row>
+            <Row>
+                <Col sm={12} className="d-flex flex-column justify-content-center">
+                        <div className="my-auto">
                     {
                         currentService !== '' ?
-                        <div>
+                        <>
                             <p className="mt-auto text-center"><strong>People in queue: {queueLength}</strong></p>
-                            <p className="mb-auto text-center"><strong>Estimated waiting time: {moment.utc(moment.duration(waitingTime, 'minutes').asMilliseconds()).format("H[h] m[m]")}</strong></p>
-                        </div>
+                        </>
                         :
                             <p className="my-auto text-center">Select a service to get more information</p>
-                    }
+                        }
+                        </div>
                     
                 </Col>
             </Row>
-            <Row className='fixed-bottom'>
+            <Row>
                 <Button disabled={currentService === '' ? true : false} className="my-2 mx-auto">
                     Get the ticket
                 </Button>
