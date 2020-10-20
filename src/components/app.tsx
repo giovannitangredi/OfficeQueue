cea7d5e8229c82021f546a9784a0edeb5d7c1a6f
Federico Quarta <s280113@studenti.polito.it>
2020-10-20 18:33:15 +0200

    add officer route

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,11 +4,10 @@ import axios from 'axios'
 import NextPerson from "./NextPerson"
 import ServiceSelectionView from './ServiceSelectionView';
 import { Navbar, Container, Row } from 'react-bootstrap';
-
+import { Switch, Route } from 'react-router-dom';
 
 // Import styles
 import './../styles/styles.css'
-import Switch from 'react-bootstrap/esm/Switch';
 
 // Create App component
 export const App = () => {
@@ -16,9 +15,7 @@ export const App = () => {
 
   return (
     <div className="app">
-      <NextPerson currentNumber="2" currentType="Pay" />
-      <Container fluid className='mx-0 p-0' >
-      <Row className='col-12 m-0 p-0' style={{ height: '10vh' }}>
+       <Row className='col-12 m-0 p-0' style={{ height: '10vh' }}>
         <Navbar expand="md" bg="light" className='col-12'>
         <div className="d-flex justify-content-between align-items-center flex-wrap w-100">
           <h4 className="align-middle font-weight-bold ml-2 mb-0">Office Queue</h4>
@@ -26,9 +23,17 @@ export const App = () => {
         </Navbar>
       </Row>
       <Row className='col-12 m-0 p-0' style={{ height: '90vh' }}>
-        <ServiceSelectionView/>
+        <Switch>
+          <Route path="/officer">
+            <NextPerson currentNumber="2" currentType="Pay" />
+          </Route>
+          <Route>
+            <Container fluid className='mx-0 p-0' >
+              <ServiceSelectionView/>
+            </Container>
+          </Route>
+        </Switch>
       </Row>
-    </Container>
     </div>
     
   )
