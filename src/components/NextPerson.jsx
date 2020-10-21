7eea8e5a131645966dd3aff8e700044b146668e7
Behnam Lotfi <behnam.lotfi@studenti.polito.it>
2020-10-21 20:10:40 +0200

    fix: added ability to get current ticket and serve the customer

--- src/components/NextPerson.jsx
+++ src/components/NextPerson.jsx
@@ -1,63 +1,118 @@
+import React from "react";
 
-import React from 'react';
+import ReactBootstrap, { Button, Col, Row } from "react-bootstrap";
+import axios from "axios";
 
-import ReactBootstrap,{Button,Col, Row} from 'react-bootstrap'
-import axios from 'axios'
+class NextPerson extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentType: "Shipping",
+      currentSID: 1,
+      currentCID: 1,
+      currentTID: 0,
+    };
+    this.handleNext();
+  }
 
-class NextPerson extends React.Component{
-constructor (props){
-super(props);
-let {currentNumber,currentType,currentSID,currentCID,next_handler}=props;
-}
 
 
-handleNext=(evt)=>{
+  
+  handleNext = (evt) => {
+      if(evt)
     evt.preventDefault();
-    if(this.props.next_handler!=undefined && this.props.next_handler)
-    this.props.next_handler();
-    axios   //call new customer
-            .get('http://localhost:4001/tickets/nextperson',
+    axios //call new customer
+      .get("http://localhost:4001/tickets/nextperson", {
+        params: {
+          CID: this.state.currentSID,
+        },
+      })
+      .then((response) => {
+        let result = response.data.reduce((acc, er) =>   er.TID, 0);
+        this.setState({ currentTID: result });
+        console.log("TID="+this.state.currentTID+" SID="+this.state.currentSID);
+       
+      })
+      .catch((error) =>
+        console.error(`There was an error retrieving the ticket list: ${error}`)
+      );
+      axios   //update ticket status and counter
+            .post('http://localhost:4001/tickets/updateticket',
             {
-                params: {
-                    CID: this.props.currentSID,
-                }
+                    TID: this.state.currentTID,
+                    Status: 'C',
+                    CID:this.state.currentSID, 
+                
             })
             .then(response => {
-                console.log(response.data)
+                
             })
-            .catch(error => console.error(`There was an error retrieving the ticket list: ${error}`))
-}
+            .catch(error => console.error(`There was an error retrieving the ticket list: ${error}`));
 
-render(){
-    return(
-        <div className="d-flex w-100 justify-content-between">
+
+    //     this.setState({ currentNumber: this.state.currentSID , currentType:"Payment"});
+  };
+  onChangeText = (name,value) => {
+   
+   console.log(value);
+    this.setState({ currentCID: value });
+   
+
+  };
+
+  render() {
+    return (
+      <div className="d-flex w-100 justify-content-between">
         <div className="container">
-            <Row>
-           
+          <Row>
+            <label className="col-lg-12">Counter ID</label>
+            <input
+              name="Counterinput"
+              type="number"
+              className="alert alert-info digit7style col-lg-12"
+              value={this.state.currentCID}
+              onChange={(ev) => this.onChangeText(ev.target.name, ev.target.value)}
+             
+            />
+          </Row>
+          <Row>
             <label className="col-lg-12">Current Number</label>
-        <input name="digitshow" type="text" className="alert alert-info digit7style col-lg-12" value={this.props.currentNumber} disabled />
-        </Row> 
-        <Row>
-        <label  className="col-lg-12">Current Type</label>
-        <input name="Typeshow" type="text" className="alert alert-info col-lg-12" value={this.props.currentType}  disabled />
-
-        
-      </Row>
-        <Row>
-       
-        <Col> </Col>
-        <Col>
-            <Button variant="primary" type="Button" className="btn btn-block" onClick={(event) => this.handleNext(event)}>Next</Button>
-       </Col> 
-       <Col> </Col>
-          
-     
-        </Row> 
-        </div> 
+            <input
+              name="digitshow"
+              type="text"
+              className="alert alert-info digit7style col-lg-12"
+              value={this.state.currentTID}
+              disabled
+            />
+          </Row>
+          <Row>
+            <label className="col-lg-12">Current Type</label>
+            <input
+              name="Typeshow"
+              type="text"
+              className="alert alert-info col-lg-12"
+              value={this.state.currentType}
+              disabled
+            />
+          </Row>
+          <Row>
+            <Col> </Col>
+            <Col>
+              <Button
+                variant="primary"
+                type="Button"
+                className="btn btn-block"
+                onClick={(event) => this.handleNext(event)}
+              >
+                Next
+              </Button>
+            </Col>
+            <Col> </Col>
+          </Row>
         </div>
-
-
-)}
+      </div>
+    );
+  }
 }
 
 export default NextPerson;
