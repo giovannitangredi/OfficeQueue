192689b6bf8d9e12bcbec41d85aa15453d0ebe9b
Enrico Carraro <enricocarraro@google.com>
2020-10-22 10:21:45 +0200

    Add testing database configuration.

--- test/service.js
+++ test/service.js
@@ -8,8 +8,8 @@ const knex = require("../server/db");
 describe("GET /service/allservice", () => {
   beforeEach(async () => {
     await knex("service").del();
-    await knex("service").insert({ Type: "Shipping", Time: "10"})
-    await knex("service").insert({ Type: "Money Transfer ", Time: "5"})
+    await knex("service").insert({ Type: "Shipping", Time: "10" });
+    await knex("service").insert({ Type: "Money Transfer ", Time: "5" });
   });
   it("should return 3 results with status 200", async () => {
     let res = await chai.request(app).get("/service/allservice").send();
@@ -24,7 +24,6 @@ describe("GET /service/allservice", () => {
 
 describe("GET /service/serviceticket", () => {
   it("should return 'related Service.' with status 200", async () => {
-  
     res = await chai
       .request(app)
       .get("/service/serviceticket")
@@ -33,5 +32,4 @@ describe("GET /service/serviceticket", () => {
     expect(res.status).to.equal(200);
     expect(res.body.length).to.equal(1);
   });
-
 });
--- test/ticket.js
+++ test/ticket.js
@@ -40,7 +40,6 @@ describe("POST /tickets/createticket", () => {
   });
 });
 
-
 describe("POST /tickets/updateticket", () => {
   beforeEach(async () => {
     await knex("ticket").del();
@@ -70,12 +69,12 @@ describe("POST /tickets/updateticket", () => {
     expect(res.body.message).to.equal("Ticket updated.");
 
     res = await chai
-    .request(app)
-    .post("/tickets/updateticket")
-    .send({ TID: 6, CID: 3, Status: "C" });
+      .request(app)
+      .post("/tickets/updateticket")
+      .send({ TID: 6, CID: 3, Status: "C" });
 
-  expect(res.status).to.equal(200);
-  expect(res.body.message).to.equal("Ticket updated.");
+    expect(res.status).to.equal(200);
+    expect(res.body.message).to.equal("Ticket updated.");
 
     expect(res.status).to.equal(200);
 
@@ -86,15 +85,12 @@ describe("POST /tickets/updateticket", () => {
 
     expect(res.status).to.equal(200);
     expect(res.body.length).to.equal(2);
-
   });
 
   afterEach(async () => {
     await knex("ticket").del();
   });
 });
-
-
 
 describe("GET /tickets/nextperson", () => {
   beforeEach(async () => {
@@ -107,8 +103,8 @@ describe("GET /tickets/nextperson", () => {
     await knex("ticket").insert({ SID: 2, Status: "A" });
     await knex("ticket").insert({ SID: 2, Status: "A" });
     await knex("ticket").insert({ SID: 2, Status: "C" });
-    await knex("service").insert({ Type: "Shipping", Time: "10"})
-    await knex("service").insert({ Type: "Money Transfer ", Time: "5"})
+    await knex("service").insert({ Type: "Shipping", Time: "10" });
+    await knex("service").insert({ Type: "Money Transfer ", Time: "5" });
   });
   it("should return the right ticket for next customer", async () => {
     let res = await chai
@@ -118,37 +114,28 @@ describe("GET /tickets/nextperson", () => {
 
     expect(res.status).to.equal(200);
     expect(res.body.length).to.equal(1);
-    expect(res.body[0].TID).to.equal(1);  //ticket 1 extracted
-
-    res = await chai
-    .request(app)
-    .post("/tickets/updateticket")
-    .send({ TID: 1, CID: 1, Status: "C" });
+    expect(res.body[0].TID).to.equal(1); //ticket 1 extracted
 
     res = await chai
       .request(app)
-      .get("/tickets/nextperson")
-      .query({ CID: 1 });
+      .post("/tickets/updateticket")
+      .send({ TID: 1, CID: 1, Status: "C" });
+
+    res = await chai.request(app).get("/tickets/nextperson").query({ CID: 1 });
 
     expect(res.status).to.equal(200);
     expect(res.body.length).to.equal(1);
-    expect(res.body[0].TID).to.equal(2);  //ticket 2 extracted
+    expect(res.body[0].TID).to.equal(2); //ticket 2 extracted
 
-    res = await chai
-    .request(app)
-    .post("/tickets/updateticket")
-    .send({ TID: 2, CID: 1, Status: "C" });
-    
     res = await chai
       .request(app)
-      .get("/tickets/nextperson")
-      .query({ CID: 1 });
+      .post("/tickets/updateticket")
+      .send({ TID: 2, CID: 1, Status: "C" });
+
+    res = await chai.request(app).get("/tickets/nextperson").query({ CID: 1 });
 
     expect(res.status).to.equal(200);
-    expect(res.body[0].TID).to.equal(5);  //ticket 5 extracted (lowest service)
-    
-    
-
+    expect(res.body[0].TID).to.equal(5); //ticket 5 extracted (lowest service)
   });
 
   afterEach(async () => {
@@ -156,8 +143,6 @@ describe("GET /tickets/nextperson", () => {
     await knex("service").del();
   });
 });
-
-
 
 describe("GET /tickets/filterticket", () => {
   beforeEach(async () => {
