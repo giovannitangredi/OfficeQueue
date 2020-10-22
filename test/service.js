04902c13eae16de38b69b3111a0aaba25bd66d5e
Luca Viscanti <s277981@studenti.polito.it>
2020-10-22 11:54:31 +0200

    fix: serviceticket

--- test/service.js
+++ test/service.js
@@ -23,13 +23,36 @@ describe("GET /service/allservice", () => {
 });
 
 describe("GET /service/serviceticket", () => {
+  beforeEach(async () => {
+    await knex("service").del();
+    await knex("service").insert({ Type: "Shipping", Time: "10" });
+    await knex("service").insert({ Type: "Money Transfer", Time: "5" });
+    await knex("ticket").del();
+    await knex("ticket").insert({ SID: 1, Status: "A" });
+    await knex("ticket").insert({ SID: 2, Status: "S" });
+  });
   it("should return 'related Service.' with status 200", async () => {
     res = await chai
       .request(app)
       .get("/service/serviceticket")
-      .query({ SID: 1 });
+      .query({ TID: 1 });
 
     expect(res.status).to.equal(200);
     expect(res.body.length).to.equal(1);
+    expect(res.body[0].Type).to.equal("Shipping")
+
+    res = await chai
+      .request(app)
+      .get("/service/serviceticket")
+      .query({ TID: 2 });
+
+    expect(res.status).to.equal(200);
+    expect(res.body.length).to.equal(1);
+    expect(res.body[0].Type).to.equal("Money Transfer")
+
+  });
+  afterEach(async () => {
+    await knex("ticket").del();
+    await knex("service").del();
   });
 });
